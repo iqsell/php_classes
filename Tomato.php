bf18620b0c161d2605aecebca3373766376654b7
iqsell <91834688+iqsell@users.noreply.github.com>
2024-05-04 12:11:58 +0300

    Update Tomato.php

--- Tomato.php
+++ Tomato.php
@@ -8,37 +8,12 @@ class Tomato
     const RED_TOMATO = 3;
 
 
-
     private int $index;
     private int $state;
 
     public function __construct(int $index)
     {
         $this->index = $index;
-        $this->state = self::NOTHING;
-    }
-
-    public function grow(): void
-    {
-        switch ($this->state) {
-            case self::NOTHING:
-                $this->state = self::FLOWER;
-                break;
-            case self::FLOWER:
-                $this->state = self::GREEN_TOMATO;
-                break;
-            case self::GREEN_TOMATO:
-                $this->state = self::RED_TOMATO;
-                break;
-        }
-    }
-
-    public function isRipe(): bool
-    {
-        return $this->state === self::RED_TOMATO;
-    }
-}
-
         $this->state = self::NOTHING;
     }
 
